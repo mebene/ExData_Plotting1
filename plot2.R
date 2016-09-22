78ccaf35c36d4206d840226d34905bb0e15fbab6
Ebene <emboumi@yahoo.com>
2016-09-22 01:08:02 -0500

    adding read data

--- plot2.R
+++ plot2.R
@@ -1,4 +1,16 @@
-source("format_data.R")
+## read data
+hpc <- read.table("household_power_consumption.txt",sep=";", header = TRUE)
+## change fromat time as Posix using strptime then converting to time only using strftime
+hpc$Time <- strptime(hpc$Time,format = "%H:%M:%S")
+hpc$Time <- strftime(hpc$Time, format = "%H:%M:%S")
+
+## change format to date using As.Date
+hpc$Date <- as.Date(hpc$Date,"%d/%m/%Y")
+
+##subset the data with only dates 2007-02-01 & 2007-02-02
+hpc_sub <- subset(hpc, Date == "2007-02-01" | Date == "2007-02-02")
+
+
 
 ## create graphing png file
  png(filename="plot2.png", height = 480 , width = 480)
